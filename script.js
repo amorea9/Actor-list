eada2d11abd708ea7e4da760604cbc5ca8aa15ae
Alessia <a.amore9393@gmail.com>
2022-08-23 23:02:54 +0200

    javascript working and more style

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch("actors.json")
+  .then((res) => res.json())
+  .then((data) => handleActor(data));
+
+function handleActor(data) {
+  console.log(data);
+  data.forEach(showActor);
+}
+
+function showActor(actor) {
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+  clone.querySelector(".name").textContent = actor.fullname;
+  clone.querySelector(".movie").textContent = actor.movie;
+  const parent = document.querySelector(".actors_container");
+  parent.appendChild(clone);
+}
