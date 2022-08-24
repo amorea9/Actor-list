83325f8bdd364344d72533ff7ff20f9c734c2e07
Alessia <a.amore9393@gmail.com>
2022-08-24 23:25:30 +0200

    pop up working

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const popUp = document.querySelector(".actors_pop_up");
+let actor;
+
 fetch("actors.json")
   .then((res) => res.json())
   .then((data) => handleActor(data));
@@ -10,14 +13,17 @@ function handleActor(data) {
 function showActor(actor) {
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
-  clone.querySelector(".name").textContent = actor.fullname;
-  clone.querySelector(".movie").textContent = actor.movie;
+  clone.querySelector(".name").textContent = `Name: ${actor.fullname}`;
+  clone.querySelector(".movie").textContent = `Appeared in: ${actor.movie}`;
+  clone.querySelector(".actors_card").addEventListener("click", OpenPopup);
+
   const parent = document.querySelector(".actors_container");
   parent.appendChild(clone);
-}
 
-document.querySelector(".actors_card").addEventListener("click", popUp);
-//or alert?
-function popUp() {
-  console.log("hello");
+  function OpenPopup() {
+    document.querySelector(".actors_pop_up").classList.toggle("open");
+    console.log("card being clicked");
+    popUp.querySelector(".name_pop_up").textContent = actor.fullname;
+    popUp.querySelector(".movie_pop_up").textContent = actor.movie;
+  }
 }
